7796bfbdade17df9f8a1653a0fd838057f806097
pooriya <adibradp@gmail.com>
2024-07-14 17:27:00 +0330

    login page be set and debug it

--- ecommerce/account/views.py
+++ ecommerce/account/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.contrib import messages
 # Create your views here.
+def Login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'login be successful')
+            return redirect('login')
+        else:
+            messages.success(request, 'we have trouble! please try again later!')
+            return redirect('login')
+    else:
+        return render(request, 'login.html')

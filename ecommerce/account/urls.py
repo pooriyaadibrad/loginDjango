c468948de2cb942ac69b96c6d9ac5c2ba0eebfb7
Majid <majidh2008@yahoo.com>
2024-07-19 22:09:13 +0330

    I added Signup and Logout

--- ecommerce/account/urls.py
+++ ecommerce/account/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('login/',views.Login,name='login')
+    path('login/',views.Login,name='login'),
+    path('logout/',views.Logout,name='logout'),
+    path('signup/',views.signup,name='signup'),
+
 ]
--- ecommerce/account/views.py
+++ ecommerce/account/views.py
@@ -17,3 +17,29 @@ def Login(request):
             return redirect('login')
     else:
         return render(request, 'login.html')
+
+def signup(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+        if password == confirm_password:
+            if User.objects.filter(username=username).exists():
+                messages.error(request, 'Username already taken!')
+            else:
+                user = User.objects.create_user(username=username, password=password)
+                user.save()
+                messages.success(request, 'Account created!')
+                return redirect('login')
+        else:
+            messages.error(request, 'Passwords do not match!')
+        return redirect('login')
+    else:
+        return render(request, 'login.html')
+
+def Logout(request):
+    logout(request)
+    messages.success(request, 'Logged out successfully')
+    return redirect('login')
+
+
